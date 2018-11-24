ba51f02f06520cb3a533f9bd174a28523f7c864a
Ferruccio Barletta <ferruccio.barletta@gmail.com>
2018-11-23 19:50:27 -0500

    Move name & keyword lists into separate files

--- build.rs
+++ build.rs
@@ -1,103 +1,41 @@
 extern crate phf_codegen;
 
+use std::collections::hash_set::HashSet;
 use std::env;
 use std::fs::File;
-use std::io::{BufWriter, Write};
+use std::io::{BufRead, BufReader, BufWriter, Write};
 use std::path::Path;
 
 fn main() {
-    let names = [
-        "Unknown",
-        "AA",
-        "AcroForm",
-        "Ascent",
-        "ASCII85Decode",
-        "ASCIIHexDecode",
-        "CCITTFaxDecode",
-        "Catalog",
-        "Collection",
-        "Crypt",
-        "DCTDecode",
-        "Dests",
-        "DL",
-        "Encrypt",
-        "Extensions",
-        "FlateDecode",
-        "Font",
-        "FontDescriptor",
-        "FontFile",
-        "ID",
-        "Info",
-        "JBIG2Decode",
-        "JPXDecode",
-        "Lang",
-        "Legal",
-        "Length",
-        "LZWDecode",
-        "MarkInfo",
-        "Metadata",
-        "Names",
-        "NeedsRendering",
-        "OCProperties",
-        "OpenAction",
-        "Outlines",
-        "OutputIntents",
-        "Page",
-        "Pages",
-        "PageLabels",
-        "PageLayout",
-        "PageMode",
-        "Perms",
-        "PieceInfo",
-        "Prev",
-        "Requirements",
-        "Root",
-        "RunLengthDecode",
-        "Size",
-        "SpiderInfo",
-        "StructTreeRoot",
-        "SubType",
-        "Threads",
-        "ToUnicode",
-        "Type",
-        "URI",
-        "Version",
-        "ViewerPreferences",
-    ];
-    generate("codegen_names.rs", "NAMES", "PdfName", &names[..]);
+    let names = load("names.txt");
+    generate("codegen_names.rs", "NAMES", "PdfName", names);
 
-    let keywords = [
-        "Unknown",
-        "endobj",
-        "endstream",
-        "f",
-        "false",
-        "n",
-        "null",
-        "obj",
-        "R",
-        "stream",
-        "trailer",
-        "true",
-        "startxref",
-        "xref",
-    ];
-    generate(
-        "codegen_keywords.rs",
-        "KEYWORDS",
-        "PdfKeyword",
-        &keywords[..],
-    );
+    let keywords = load("keywords.txt");
+    generate("codegen_keywords.rs", "KEYWORDS", "PdfKeyword", keywords);
 }
 
-fn generate(filename: &str, target: &str, typename: &str, entries: &[&str]) {
+fn load(filename: &str) -> Vec<String> {
+    let mut strings = HashSet::<String>::new();
+    strings.insert("Unknown".to_owned());
+    let of = File::open(filename).unwrap();
+    let file = BufReader::new(&of);
+    for (_, line) in file.lines().enumerate() {
+        let l = line.unwrap().trim().to_owned();
+        if l.len() > 0 && l.chars().next().unwrap() != '#' {
+            strings.insert(l.to_owned());
+        }
+    }
+    strings.into_iter().collect()
+}
+
+fn generate(filename: &str, target: &str, typename: &str, entries: Vec<String>) {
     let path = Path::new(&env::var("OUT_DIR").unwrap()).join(filename);
     let mut file = BufWriter::new(File::create(&path).unwrap());
 
     writeln!(&mut file, "#[derive(Debug, Clone, PartialEq, Eq, Hash)]");
     writeln!(&mut file, "#[allow(non_camel_case_types)]");
     writeln!(&mut file, "pub enum {} {{", typename);
-    for &entry in entries {
+    for entry in entries.iter() {
         writeln!(&mut file, "    r#{},", entry);
     }
     writeln!(&mut file, "}}\n");
@@ -108,8 +46,8 @@ fn generate(filename: &str, target: &str, typename: &str, entries: &[&str]) {
         target, typename
     ).unwrap();
     let mut builder = phf_codegen::Map::new();
-    for &entry in entries {
-        builder.entry(entry, &format!("{}::r#{}", typename, entry));
+    for entry in entries.iter() {
+        builder.entry(&entry[..], &format!("{}::r#{}", typename, entry));
     }
     builder.build(&mut file).unwrap();
     writeln!(&mut file, ";").unwrap();
